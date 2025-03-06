8f792e8a51f764fb4ce1e9675088a57094c8bf96
notAl1ve <pakish2132@mail.ru>
2025-03-07 01:45:37 +0300

    NextFIx

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
         container: document.querySelector("#container"),
-        imageTargetSrc: "ArProjcete/marker.mind",
+        imageTargetSrc: "./ArProjcete/marker.mind",
     });
 
     const { renderer, scene, camera} = mindarThree;
     const anchor = mindarThree.addAnchor(0);
 
     const loader = new THREE.GLTFLoader();
-    loader.load("ArProjcete/model.glb", (gltf) => {
+    loader.load("./ArProjcete/model.glb", (gltf) => {
         const model = gltf.scene;
         model.scale.set(0.5, 0.5, 0.5);
         model.position.set(0, 0, 0);
